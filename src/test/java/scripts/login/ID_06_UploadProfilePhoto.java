54c97bbf345c69f16276b1a2304df537a2419238
ac-amruta12 <amruta.ac12@gmail.com>
2024-04-08 20:00:32 +0530

    Added Test of upload profile photo

---
+++ src/test/java/scripts/login/ID_06_UploadProfilePhoto.java
@@ -0,0 +1,56 @@
+package scripts.login;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import utils.DataInputProvider;
+import wrapper.GenericWrappers;
+
+public class ID_06_UploadProfilePhoto extends GenericWrappers {
+	
+	@BeforeTest
+	public void setReportDetails() {
+		
+		System.out.println(" ------  in before test ------");
+		testcaseName = this.getClass().getName();
+		testcaseDec = this.getClass().getSimpleName();
+		author= "Amruta Chougule";
+		category = "Login";
+	}
+	
+	@Test(dataProvider = "LoginTestID_01")
+	public void validLoginTest(String username,String password,String expected_result)
+	{
+		try {
+			launchApp(browserName, false);
+			new LoginPage()
+			.enterUserName(username)
+			.enterPassword(password)
+			.clickonLoginBTN()
+			.verifyRememberPasswordpopupisDisplayed()
+			.verifyHomepageDisplayed(expected_result)
+			.clickOnProfileIcon()
+			.verifyProfileNameDisplayed(expected_result);
+			
+			Status= "PASS";
+		}
+		
+		finally {
+			quitBrowser();
+		}
+		
+	}
+	
+	@DataProvider(name= "LoginTestID_01")
+	public Object[][] fetchData() throws IOException{
+		Object[][] arrayObj = DataInputProvider.getSheet("LoginTest_ID_01");
+		return arrayObj;
+	}
+
+
+
+}
