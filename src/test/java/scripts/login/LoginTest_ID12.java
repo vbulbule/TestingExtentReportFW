6591290aeb90f80cc8197587f829618cb58d0560
vbulbule <vitthalbulbule94@gmail.com>
2024-04-10 22:01:21 +0530

    vitthals code

---
+++ src/test/java/scripts/login/LoginTest_ID12.java
@@ -0,0 +1,19 @@
+package scripts.login;
+
+import org.testng.annotations.BeforeTest;
+
+import wrapper.GenericWrappers;
+
+public class LoginTest_ID12 extends GenericWrappers{
+	
+	@BeforeTest
+	public void setReportDetails() {
+		
+		System.out.println(" ------  in before test ------");
+		testcaseName = this.getClass().getName();
+		testcaseDec = this.getClass().getSimpleName();
+		author= "Amruta Chougule";
+		category = "Login";
+	}
+
+}
